3a2c964be333f8f7feecef9d658af4b4c78497cb
LucasPorfirioDaSilva <lucasporfirio1997@gmail.com>
2018-03-20 07:12:10 -0300

    adicionando models

---
+++ lanchonete/Models/PedidoProduto.cs
@@ -0,0 +1,11 @@
+namespace lanchonete.Models
+{
+    public class PedidoProduto
+    {
+        public long id { get; set; }
+        public long Pedidoid { get; set; }
+        public Pedido pedido { get; set; }
+        public long Produtoid { get; set; }   
+        public int  pqtde { get; set; }
+    }
+}
